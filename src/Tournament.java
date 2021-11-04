2c7e4751ecca4eb75d8a4cd79a3526bca6c29300
MuneebAshraf <Munne_122@hotmail.com>
2021-11-04 12:20:44 +0100

    Added attributes and created Player class

---
+++ src/Tournament.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Tournament {
+
+    private String name;
+    private ArrayList<Team> teams;
+    private Knockout knockout;
+    private String deadline;
+    private boolean closeSignUp;
+    //maybe private date
+    //private Date deadline
+}
