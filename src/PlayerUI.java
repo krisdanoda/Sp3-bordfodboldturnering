8cec36cdda78582b2d819a4bb0a7c08f887b6dad
SMerved <90383268+SMerved@users.noreply.github.com>
2021-11-08 11:52:23 +0100

    Update

--- src/PlayerUI.java
+++ src/PlayerUI.java
@@ -1,69 +1,52 @@
 import java.util.ArrayList;
 
 public class PlayerUI {
-    public static void playerMenu(){
-        Tournament tournament=new Tournament("Bordfodbold-knockoutturnering") ;
+    public static void playerMenu() {
+        Tournament tournament = new Tournament("Bordfodbold-knockoutturnering");
 
 
-        boolean closeSignup=false ;
+        boolean closeSignup = false;
 
-        if(closeSignup){
+        if (closeSignup) {
             System.out.println("Der er lukket for tilmelding");
-        }
+        } else {
+            int input = 0;
 
-        else{
-            int input=0 ;
+            input = UI.getUserInputInt("1. Opret nyt hold\n2. Tilmeld spiller til eksisterende hold");
 
-            input=UI.getUserInputInt("1. Opret nyt hold\n2. Tilmeld spiller til eksisterende hold") ;
-
-            if(input==1) {
-                int input2=0 ;
+            if (input == 1) {
+                int input2 = 0;
                 String inputS = "";
 
                 inputS = UI.getUserInput("Angiv holdnavn:");
-                Team team1 = new Team(inputS);
-                tournament.getTeams().add(team1);
+                Team team = new Team(inputS);
+                tournament.getTeams().add(team);
 
                 inputS = UI.getUserInput("Angiv navn på første spiller:");
-                team1.getPlayers().add(new Player(inputS));
+                team.getPlayers().add(new Player(inputS));
 
                 inputS = UI.getUserInput("Angiv navn på næste spiller");
-                team1.getPlayers().add(new Player(inputS));
+                team.getPlayers().add(new Player(inputS));
 
                 input2 = UI.getUserInputInt("1.Fuldfør tilmelding\n2. Tilføj flere spillere");
                 if (input2 == 1) {
-                    System.out.println("Jeres hold " + team1.getName() + " er nu tilmeldt " + tournament.getName() + "en");
-                }
-                else if (input2 == 2) {
+                    System.out.println("Jeres hold " + team.getName() + " er nu tilmeldt " + tournament.getName() + "en");
+                } else if (input2 == 2) {
                     inputS = UI.getUserInput("Angiv navn på næste spiller");
-                    team1.getPlayers().add(new Player(inputS));
+                    team.getPlayers().add(new Player(inputS));
 
                     input2 = UI.getUserInputInt("1.Fuldfør tilmelding\n2. Tilføj flere spillere");
                     if (input2 == 1) {
-                        System.out.println("Jeres hold " + team1.getName() + " er nu tilmeldt " + tournament.getName() + "en");
-                    }
-                    else if (input2 == 2) {
+                        System.out.println("Jeres hold " + team.getName() + " er nu tilmeldt " + tournament.getName() + "en");
+                    } else if (input2 == 2) {
                         inputS = UI.getUserInput("Angiv navn på sidste spiller");
-                        team1.getPlayers().add(new Player(inputS));
-                        System.out.println("Jeres hold " + team1.getName() + " er nu tilmeldt " + tournament.getName() + "en");
+                        team.getPlayers().add(new Player(inputS));
+                        System.out.println("Jeres hold " + team.getName() + " er nu tilmeldt " + tournament.getName() + "en");
                     }
                 }
-            }
-            else if(input==2){
+            } else if (input == 2) {
 
                 //Tilføj spiller til eksisterende
-                for(int i=0 ; i< tournament.getTeams().size() ; i++) {
-                    int count = 1;
-                    System.out.println(count + ". " + tournament.getTeams().get(i));
-                    count++ ;
-                }
-
-                switch(UI.getUserInputInt("Hvilket hold vil du tilføje en spiller til:")){
-                    case 1 {
-
-                    }
-                }
-                
             }
         }
     }
